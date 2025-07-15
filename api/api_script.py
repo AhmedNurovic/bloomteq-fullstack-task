d8efcafa14056ee9d40e4dd6c643ceea6175464a
Ahmed Nurovic <ahmed.nurovic@hotmail.com>
2025-07-15 05:20:22 +0200

    chore: Delete unused files

--- api/api_script.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Simple test script to verify the Flask API endpoints
-"""
-
-from app import app
-
-# This file is used by Vercel to serve the Flask application
-# The app is imported from the parent directory
