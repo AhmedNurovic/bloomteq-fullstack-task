864caeb879db8c63cc7af6ca14bf3882fb25bebf
Ahmed Nurovic <ahmed.nurovic@hotmail.com>
2025-07-15 02:05:45 +0200

    fix: Jest configuration for React 19 and remove invalid Vercel runtime config

--- frontend/frontend/jest.config.cjs
+++ frontend/frontend/jest.config.cjs
@@ -1,8 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^.+\\.(svg)$': '<rootDir>/__mocks__/svgMock.js',
     '^@/(.*)$': '<rootDir>/src/$1',
@@ -15,4 +13,10 @@ module.exports = {
     '<rootDir>/src/**/*.test.ts',
     '<rootDir>/src/**/*.test.tsx'
   ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 }; 
